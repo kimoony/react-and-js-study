588cf25f273a30f6c5c496e779bcae31fc3c86dc
Kim Hoon <71738968+kimoony@users.noreply.github.com>
2022-01-22 02:59:13 +0900

    add study file Array

---
+++ src/arr.js
@@ -0,0 +1,18 @@
+// 배열
+
+let arr = [1, 2, 3, 4, 5]; // 배열 리터럴
+
+// 추가(배열의 가장 마지막에)
+arr.push(6);
+
+// console.log(arr);
+
+// 각 인덱스에 값 가져올 때
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+// 배열의 길이
+console.log(arr.length);
--- src/obj.js
+++ src/obj.js
@@ -19,7 +19,7 @@ const user = {
   name: "Hoony", // 객체 맴버
   age: 34, // 객체 맴버
   say: function () {
-    console.log(`안녕? 나는 ${this.name}이야`); // this(=user)는 객체 자신
+    // console.log(`안녕? 나는 ${this.name}이야`); // this(=user)는 객체 자신
   } // 객체 메서드 (방법)
 };
 
@@ -28,7 +28,7 @@ const user = {
 // // 프로퍼티의 key를 [] 안에 반드시 문자열 형태로 넣어야 한다.
 
 // 호이스팅
-console.log(getPropertyValue("name"));
+// console.log(getPropertyValue("name"));
 
 function getPropertyValue(key) {
   return user[key];
@@ -42,18 +42,18 @@ user.location = "화성시";
 user.name = "김훈";
 user["age"] = 99;
 
-console.log(user);
+// console.log(user);
 
 // 삭제
 // delete를 사용하면 메모리는 그대로 사용되기 때문에
 user.name = null; // 을 사용해 값을 비워준다.
 
 // 갹채 언애 험수
-user.say();
-user["say"]();
+// user.say();
+// user["say"]();
 
 // 객체 안에 property 확인하는 방법 - in
-console.log(`name: ${"name" in user}`); // true
-console.log(`age: ${"age" in user}`); // true
-console.log(`gender: ${"gender" in user}`); // false
+// console.log(`name: ${"name" in user}`); // true
+// console.log(`age: ${"age" in user}`); // true
+// console.log(`gender: ${"gender" in user}`); // false
 // user라는 객체 안에 name이라는 프로퍼티가 있는지 확인
