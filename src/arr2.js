62a6e84026e3773cc427bc507b07601c8f7ad3a7
Kim Hoon <71738968+kimoony@users.noreply.github.com>
2022-01-23 01:38:51 +0900

    add study file arr2-02

--- src/arr2.js
+++ src/arr2.js
@@ -30,11 +30,11 @@ const arr = [1, 2, 3, 4];
 
 // 베열 내장함수5
 const arr2 = [
-  { color: "red" },
-  { color: "black" },
-  { color: "blue" },
-  { color: "yellow" },
-  { color: "black" }
+  { num: 1, color: "red" },
+  { num: 2, color: "black" },
+  { num: 3, color: "blue" },
+  { num: 4, color: "yellow" },
+  { num: 5, color: "blue" }
 ];
 
 // findIndex는 객체배열에서 원하는 요소의 인덱스를 구할 때
@@ -42,8 +42,74 @@ const arr2 = [
 // const idx = arr2.findIndex((el) => el.color === "blue");
 // console.log(idx);
 
-// 베열 내장함수6
+// // 베열 내장함수6
 
-// find는 객체배열에서 원하는 요소를 구할 때
-const element = arr2.find((el) => el.color === "blue");
-console.log(element);
+// // find는 객체배열에서 원하는 요소를 구할 때
+// const element = arr2.find((el) => el.color === "blue");
+// console.log(element);
+
+// // 베열 내장함수7
+
+// // 필요한 요소를 필터할 때
+// const filter = arr2.filter((el) => el.color === "blue");
+// console.log(filter);
+
+// 베열 내장함수8
+
+// 배열 자르기
+// console.log(arr2.slice(1, 3));
+// slice(비긴 - 시작할 인덱스, 엔드 - 엔드에서 하나를 뺀 값까지만 표시)
+
+// // 베열 내장함수9
+// const arr3 = [
+//   { num: 1, color: "red" },
+//   { num: 2, color: "black" },
+//   { num: 3, color: "blue" }
+// ];
+// const arr4 = [
+//   { num: 4, color: "yellow" },
+//   { num: 5, color: "blue" }
+// ];
+// // 배열 붙이기
+// console.log(arr3.concat(arr4));
+
+// 베열 내장함수10
+// 문자정렬
+// let chars = ["나", "다", "가"];
+
+// chars.sort(); // 원본 배열을 정렬한다.
+// console.log(chars);
+
+// 숫자정렬
+// let num = [0, 6, 20, 50, 3];
+
+// 방법 1
+// num.sort((a, b) => a - b); // b - a 를 리턴하면 역순
+
+// 방법2
+
+// const compare = (a, b) => {
+//   if(a > b) {
+//     return 1;
+//   } else if(a < b) {
+//     return -1;
+//   } else {
+//     return 0;
+//   }
+// }
+
+// num.sort(compare)
+
+// console.log(num);
+
+// 베열 내장함수10
+// const arr5 = ["김훈", "님", "안녕하세요", "반갑습니다"];
+
+// // join() 문자열 합치기
+// console.log(arr5.join());
+// // 김훈,님,안녕하세요,반갑습니다  => 쉼표로 구분
+// // join('이곳에 구분할 기호나 공백을 넣으면 구분이 넣은 값으로 바뀜')
+// console.log(arr5.join(" "));
+// // 김훈 님 안녕하세요 반갑습니다
+// console.log(arr5.join("+"));
+// // 김훈+님+안녕하세요+반갑습니다
